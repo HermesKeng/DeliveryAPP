74df86b7a32b99af4ec0e3784825384442b81b43
HermesKeng <keng@keng-MacBook-Air.local>
2017-04-28 02:07:38 +0800

    <0428 Update> 新增cardview介面
    1.增加預設UI的卡片內容
    2.CardView的使用(Google API)

--- app/src/main/java/com/example/keng/mainpage/MainActivity.java
+++ app/src/main/java/com/example/keng/mainpage/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.keng.mainpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mlayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
