1705803a0e21b7ca22d5604c7bfba1615efb52bf
HermesKeng <keng@keng-MacBook-Air.local>
2017-05-02 21:16:15 +0800

    <0502-2 更新>LoginPage
    1.完成APP LoginPage
    2.圖片參考網址：https://goo.gl/iDxEy4

--- main/app/src/main/java/com/example/keng/main/IntroSlider.java
+++ main/app/src/main/java/com/example/keng/main/IntroSlider.java
@@ -48,8 +48,8 @@ public class IntroSlider extends AppCompatActivity {
         viewPager.setCurrentItem(0);
         //使用radioGroup將index得以顯示
         indicator=(RadioGroup)findViewById(R.id.indicator);
-        indicator.check(0);
-        radioButton.setChecked(true);
+        indicator.check(R.id.index1);
+       // radioButton.setChecked(true);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -83,9 +83,9 @@ public class IntroSlider extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                    Intent i=new Intent(IntroSlider.this,Login.class);
-                    startActivity(i);
-                    finish();
+                Intent i=new Intent(IntroSlider.this,Login.class);
+                startActivity(i);
+                finish();
 
             }
         });
@@ -93,7 +93,7 @@ public class IntroSlider extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(IntroSlider.this,MainActivity.class);
+                Intent i=new Intent(IntroSlider.this,Register.class);
                 startActivity(i);
                 finish();
             }
