2d9a6488112d71e0d7ea4fd3282323ba186b5b53
HermesKeng <keng@keng-MacBook-Air.local>
2017-05-08 17:07:21 +0800

    <0508 更新> 修正Cardview介面及新增資料
    1. 建立Order_Class 儲存清單資料型態
    2. Fragment3新增Button功能切換回主畫面
    3. 修正些許版面配置

--- main/app/src/main/java/com/example/keng/main/MyAdapter.java
+++ main/app/src/main/java/com/example/keng/main/MyAdapter.java
@@ -1,9 +1,12 @@
 package com.example.keng.main;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,10 +20,12 @@ import java.util.List;
 public class  MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     //利用建構子將資料清單傳進來
-    private List<String> mDataset;
+    private List<Order> mDataset;
+    private String[] location;
 
-    public  MyAdapter(List<String> data){
+    public  MyAdapter(List<Order> data,String[] resource){
         mDataset=data;
+        location=resource;
     }
     //指定item所使用的view佈局
     @Override
@@ -32,7 +37,28 @@ public class  MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     //透過position指定每個item所用到的資料
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.txtItem.setText((mDataset.get(position)));
+
+
+        holder.Start.setText(location[(mDataset.get(position).getStartPlace())]);
+        holder.Destination.setText(location[(mDataset.get(position).getFinal_place())]);
+        holder.Time.setText(mDataset.get(position).getSend_time());
+        holder.Cost.setText("NT$"+String.valueOf(mDataset.get(position).getOrder_cost()));
+        switch (mDataset.get(position).getOrder_state()) {
+            case 1:
+                holder.status.setImageResource(R.color.orderBar_unprocessed);
+                break;
+            case 2:
+                holder.status.setImageResource(R.color.orderBar_waiting);
+                break;
+            case 3:
+                holder.status.setImageResource(R.color.orderBar_waiting);
+                break;
+            case 4:
+                holder.status.setImageResource(R.color.orderBar_complete);
+                break;
+
+        }
+       // holder.Note.setText(mDataset.get(position).getNote());
     }
     //傳回清單的數量
     @Override
@@ -42,10 +68,17 @@ public class  MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     //透過View Holder定義介面和邏輯
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView txtItem;
+        public TextView Start,Destination,Time,Cost;
+        public ImageView status;
         public ViewHolder(View itemView) {
             super(itemView);
-            txtItem=(TextView)itemView.findViewById(R.id.textView);
+            Start=(TextView)itemView.findViewById(R.id.start);
+            Destination=(TextView)itemView.findViewById(R.id.Destination);
+            Time=(TextView)itemView.findViewById(R.id.time);
+            Cost=(TextView)itemView.findViewById(R.id.cost);
+            status=(ImageView)itemView.findViewById(R.id.status);
+           // Note=(TextView)itemView.findViewById(R.id.note);
+
         }
     }
 }
