a9b3e8273ba7f38c491f9da28c50eb668dac9bca
HermesKeng <keng@keng-MacBook-Air.local>
2017-05-11 14:02:23 +0800

    <0511 更新> 新增TabView
    1.透過兩個畫面分別顯示收件與寄件紀錄，其中紀錄又可以分成已完成、未完成、全部這三個分類
    2.在PagerAdapter中能夠去辨別透過一個type可以分辨收件紀錄及寄件紀錄
    3.更改class Order的內容：
    欄位資料變更為
    //起始地 終點 花費 寄送者 接收者 電話 時間 備註 密碼 狀態

--- main/app/src/main/java/com/example/keng/main/Mainpage.java
+++ main/app/src/main/java/com/example/keng/main/Mainpage.java
@@ -17,12 +17,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class Mainpage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener ,NewOrderFragment3.ListComplete{
         int index=0;
     HomeFragment homeFragment;
     NewOrderFragment newOrderFragment;
-    HistoryMainFragment historyMainFragment;
+    HistoryMainFragment historyFragment;
+    HistoryMain_RecvFragment historyMain_recvFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +42,25 @@ public class Mainpage extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //在Mainpage讀資料，一一將每一筆資料傳遞到每個fragment
+        ArrayList<Order> dataset = new ArrayList<Order>();
+
+        Order[] order=new Order[4];
+
+        order[0]=new Order(3,4,50,"張主任","耿楷寗","0933232456","2017年5月30日17點05分","","1234",1);
+        order[1]=new Order(0,2,30,"張主任","張紘綸","0921357849","2017年4月28日11點25分","記得要做考古題","5678",2);
+        order[2]=new Order(1,5,40,"耿楷寗","張主任","0932654378","2017年4月20日14點30分","內有公文記得簽收","1213",3);
+        order[3]=new Order(3,4,20,"張主任","電資院","0988960459","2017年4月17日09點25分","最新講座資訊敬請公告","1344",4);
+
+        for(int i=0;i<order.length;i++){
+            dataset.add(order[i]);
+        }
+
         if(savedInstanceState==null){
             homeFragment=new HomeFragment();
             newOrderFragment=new NewOrderFragment();
-            historyMainFragment=new HistoryMainFragment();
+            historyFragment=new HistoryMainFragment(dataset);
+            historyMain_recvFragment=new HistoryMain_RecvFragment(dataset);
             getSupportFragmentManager().beginTransaction().add(R.id.frame,homeFragment).commit();
         }
     }
@@ -67,21 +85,31 @@ public class Mainpage extends AppCompatActivity
         if(id==R.id.nav_home){
             if(index!=0){
                 switchFragment(homeFragment,index);
+                Log.d(getClass().getSimpleName()+" navi","0");
                 index=0;
             }
         }else if(id==R.id.nav_newOrder){
             if(index!=1){
                 switchFragment(newOrderFragment,index);
                 index=1;
+                Log.d(getClass().getSimpleName()+" navi","1");
             }
 
-        }else if(id==R.id.nav_history){
+        }else if(id==R.id.nav_sendhistory){
             if(index!=2){
-                switchFragment(historyMainFragment,index);
+                switchFragment(historyFragment,index);
+                Log.d(getClass().getSimpleName()+" navi","2");
                 index=2;
             }
 
-        }else if(id==R.id.nav_setting){
+        }else if(id==R.id.nav_recvhistory){
+            if(index!=3) {
+                switchFragment(historyMain_recvFragment, index);
+                Log.d(getClass().getSimpleName()+" navi","3");
+                index=3;
+            }
+        }
+        else if(id==R.id.nav_setting){
 
         }else if(id==R.id.nav_logout){
 
@@ -91,42 +119,32 @@ public class Mainpage extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    public void switchFragment(Fragment to,int index){
+    public void switchFragment(Fragment to,int index){//目的地 , 出發地
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
+
+        if(!to.isAdded()){
+            transaction.add(R.id.frame,to);
+        }
         switch (index){
             case 0:
-                if(!to.isAdded()){
-                    transaction.addToBackStack(null);
-                    transaction.add(R.id.frame,to).hide(homeFragment).commit();
-                }else {
-                    transaction.addToBackStack(null);
-                    transaction.show(to).hide(homeFragment).commit();
-                }
+                transaction.addToBackStack(null);
+                transaction.hide(homeFragment);
                 break;
             case 1:
-                if(!to.isAdded()){
-                    transaction.addToBackStack(null);
-                    transaction.add(R.id.frame,to).hide(newOrderFragment).commit();
-                }else {
-                    transaction.addToBackStack(null);
-                    transaction.show(to).hide(newOrderFragment).commit();
-                }
+                transaction.addToBackStack(null);
+                transaction.hide(newOrderFragment);
                 break;
             case 2:
-                if(!to.isAdded()){
-                    transaction.addToBackStack(null);
-                    transaction.add(R.id.frame,to).hide(historyMainFragment).commit();
-                }else {
-                    transaction.addToBackStack(null);
-                    transaction.show(to).hide(historyMainFragment).commit();
-                }
+                transaction.addToBackStack(null);
+                transaction.hide(historyFragment);
                 break;
-
-
+            case 3:
+                transaction.addToBackStack(null);
+                transaction.hide(historyMain_recvFragment);
+                break;
         }
-
-
+        transaction.show(to).commit();
     }
     @Override
     public void setIndex(int i){
